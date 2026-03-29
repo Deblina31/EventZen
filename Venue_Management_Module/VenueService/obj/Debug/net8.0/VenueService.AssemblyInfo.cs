a53bbaf604a13e73efaa20b5c553f4aae0a0b95e
Deblina Singha Roy <deblsingharoy.ext@deloitte.com>
2026-03-29 22:26:24 +0530

    Added Docker Files in correct folder

--- Venue_Management_Module/VenueService/obj/Debug/net8.0/VenueService.AssemblyInfo.cs
+++ Venue_Management_Module/VenueService/obj/Debug/net8.0/VenueService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VenueService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81011db1e080ac7046d187953a8ccaf3d26df267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04db1ebb83cd6110b32936bdb40308f273503cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VenueService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VenueService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
